6c11f6ce56469f0461301b0011165e197133a38a
Dimitar Bosnev <dimitar.bosnew@gmail.com>
2026-02-07 14:47:47 +0200

    Update

--- Assets/Scripts/WheelSpin.cs
+++ Assets/Scripts/WheelSpin.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
@@ -7,9 +8,15 @@ using UnityEngine.SceneManagement;
 public class WheelSpin : MonoBehaviour
 {
     [SerializeField, Range(0,1)] private float spin_friction = 0.99f;
-    [SerializeField] private float spin_force_min = 0.5f;
-    [SerializeField] private float spin_threshold = 1;
+    [SerializeField] private float spinForceMin = 0.5f;
+    [SerializeField] private float spinThreshold = 1;
+    [SerializeField] private float waitBeforeSeconds = 1f;
+    [SerializeField] private float waitAfterSeconds = 10f;
     [SerializeField] private List<UnityEvent> events;
+
+    [SerializeField] private GameObject factCanvas;
+    [SerializeField] private GameObject stickerCanvas;
+    [SerializeField] private GameObject nothingCanvas;
 
     private bool isRunning = false;
     private Rigidbody rb;
@@ -31,13 +38,13 @@ public class WheelSpin : MonoBehaviour
             rb.angularVelocity = new Vector3(0,0,5);
         }
 
-        if (Mathf.Abs(rb.angularVelocity.z) > spin_force_min && !isRunning)
+        if (Mathf.Abs(rb.angularVelocity.z) > spinForceMin && !isRunning)
         {
             Debug.Log("Running");
             isRunning = true;
             Collider.enabled = false;
         }
-        else if (Mathf.Abs(rb.angularVelocity.z) < spin_threshold && isRunning)
+        else if (Mathf.Abs(rb.angularVelocity.z) < spinThreshold && isRunning)
         {
             float sction_degrees = 360 / events.Count;
             int value = (int)(transform.eulerAngles.z / sction_degrees);
@@ -70,7 +77,7 @@ public class WheelSpin : MonoBehaviour
 
     public void MiniGame()
     {
-        Invoke(nameof(LoadScene), 1f);
+        Invoke(nameof(LoadScene), waitBeforeSeconds);
 
     }
     private void LoadScene()
@@ -80,31 +87,40 @@ public class WheelSpin : MonoBehaviour
 
     public void Fact()
     {
-        Invoke(nameof(ShowFact), 1f);
+        StartCoroutine(nameof(ShowFact));
     }
 
-    private void ShowFact()
+    private IEnumerator ShowFact()
     {
-        
+        yield return new WaitForSeconds(waitBeforeSeconds);
+        factCanvas.SetActive(true);
+        yield return new WaitForSeconds(waitAfterSeconds);
+        factCanvas.SetActive(false);
     }
 
     public void Sticker()
     {
-        Invoke(nameof(ShowSticker), 1f);
+        StartCoroutine(nameof(ShowSticker));
     }
 
-    private void ShowSticker()
+    private IEnumerator ShowSticker()
     {
-        
+        yield return new WaitForSeconds(waitBeforeSeconds);
+        stickerCanvas.SetActive(true);
+        yield return new WaitForSeconds(waitAfterSeconds);
+        stickerCanvas.SetActive(false);
     }
 
     public void Nothing()
     {
-        Invoke(nameof(ShowNothing), 1f);
+        StartCoroutine(nameof(ShowNothing));
     }
 
-    private void ShowNothing()
+    private IEnumerator ShowNothing()
     {
-        
+        yield return new WaitForSeconds(waitBeforeSeconds);
+        nothingCanvas.SetActive(true);
+        yield return new WaitForSeconds(waitAfterSeconds);
+        nothingCanvas.SetActive(false);
     }
 }
